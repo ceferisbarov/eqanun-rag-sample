ff8f1f664771c9a6b49390ae1eb0623d90a8785d
Jafar Isbarov <cefer.isbarov@gmail.com>
2024-06-13 05:31:18 +0400

    can now work with different laws

--- engine.py
+++ engine.py
@@ -3,7 +3,6 @@ import pickle
 import faiss
 from tqdm import tqdm
 from datasets import load_dataset
-# from huggingface_hub import login
 from sentence_transformers import SentenceTransformer
 import numpy as np
 
@@ -14,32 +13,33 @@ CHUNK_SIZE = 1024
 model = SentenceTransformer("BAAI/bge-m3")
 
 repo = "allmalab/eqanun"
+doc_id = os.environ["DOCUMENT_ID"]
 dataset = load_dataset(repo)
-labor_code = dataset["train"].filter(lambda x: x["id"] == "46943")["text"][0]
-labor_code = [labor_code[i:i+CHUNK_SIZE] for i in range(0, len(labor_code), CHUNK_SIZE)]
+doc_text = dataset["train"].filter(lambda x: x["id"] == doc_id)["text"][0]
+chunks = [doc_text[i:i+CHUNK_SIZE] for i in range(0, len(doc_text), CHUNK_SIZE)]
 
-if not os.path.exists("embeddings.pickle"):
+embedding_file = os.path.join("embeddings", f"{doc_id}.pickle")
+if os.path.exists(embedding_file):
+    with open(embedding_file, 'rb') as handle:
+        final_embeddings = pickle.load(handle)
+else:
     embeddings_list = []
-    for i in tqdm(range(0, len(labor_code), CHUNK_SIZE)):
-        text = labor_code[i:i+CHUNK_SIZE]
-        embeddings = model.encode(text)
+    for chunk in tqdm(chunks):
+        embeddings = model.encode(chunk)
         embeddings_list.append(embeddings.reshape(1,-1))
     final_embeddings = np.concatenate(embeddings_list, axis=0)
 
-    with open('embeddings.pickle', 'wb') as handle:
+    with open(embedding_file, 'wb') as handle:
         pickle.dump(final_embeddings, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-else:
-    with open('embeddings.pickle', 'rb') as handle:
-        final_embeddings = pickle.load(handle)
 
 dim = final_embeddings.shape[-1]
 index = faiss.IndexFlatL2(dim)
 index.add(final_embeddings)
 
+print(f"Index is ready. Number of entries: {index.ntotal}")
+
 def faiss_search(query, index=index, n_neighbors=5):
     query_embedding = model.encode(query).reshape(1,-1)
     distances, indices = index.search(query_embedding, n_neighbors)
 
-    return [labor_code[i] for i in indices[0]]
-
+    return [chunks[i] for i in indices[0]]
